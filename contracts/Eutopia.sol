7b852384e90824369ea61b4afe2e29443ec912c5
IqbalHossainSaidal <iqbal.hossain.saidal@gmail.com>
2024-08-01 06:23:50 -0400

    chore: Add initial project files and configurations

---
+++ contracts/Eutopia.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract Eutopia is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner) initializer public {
+        __ERC20_init("Eutopia", "EUTO");
+        __Ownable_init(initialOwner);
+    }
+}
