7906af31db69abfa7c0f2770dc406d5a455f34f9
IqbalHossainSaidal <iqbal.hossain.saidal@gmail.com>
2024-08-06 11:53:49 -0400

    chore: Add INFURA_PROJECT_ID to .env.example and hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "@nomicfoundation/hardhat-verify";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,6 +9,7 @@ dotenv.config();
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "";
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID || "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -19,6 +21,12 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
+    hardhat: {
+      chainId: 1337,
+      forking: {
+        url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      },
+    },
     sepolia: {
       url: SEPOLIA_RPC_URL,
       chainId: 11155111,
