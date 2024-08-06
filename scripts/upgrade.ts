646ed51807fc80dfcd75011497d8a939c6204aad
IqbalHossainSaidal <iqbal.hossain.saidal@gmail.com>
2024-08-06 15:34:24 -0400

    chore: Refactor deploy script to use environment variables for contract deployment

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -1,5 +1,4 @@
 import { ethers, upgrades, run } from "hardhat";
-import { getImplementationAddress } from "@openzeppelin/upgrades-core";
 
 async function main() {
     const proxyAddress = "0x9A2BAF3997A80ffCf303C8e5BB35f71c600fB362";
@@ -9,8 +8,10 @@ async function main() {
     const InstanceFactory = await ethers.getContractFactory("Eutopia");
     const upgraded = await upgrades.upgradeProxy(proxyAddress, InstanceFactory);
 
+    await upgraded.waitForDeployment();
+
     const instanceAddress = await upgraded.getAddress();
-    const implementationAddress = await getImplementationAddress(ethers.provider, instanceAddress)
+    const implementationAddress = await upgrades.erc1967.getImplementationAddress(instanceAddress);
 
     console.log("Upgraded");
     console.log("Instance address:", instanceAddress);
