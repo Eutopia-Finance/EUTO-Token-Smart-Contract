881739670ec4f856a9c043d805a9d84f3c40a664
IqbalHossainSaidal <iqbal.hossain.saidal@gmail.com>
2024-08-06 10:29:32 -0400

    chore: Update hardhat.config.ts to use environment variables for private key, RPC URL, and Etherscan API key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -10,6 +17,18 @@ const config: HardhatUserConfig = {
         enabled: true,
       },
     },
+  },
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      chainId: 11155111,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
+    }
   },
 };
 
