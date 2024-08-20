ab509710bd093198d32f4ff20a0a8fa8b598369e
IqbalHossainSaidal <iqbal.hossain.saidal@gmail.com>
2024-08-19 22:05:51 -0400

    chore: Refactor Eutopia contract to remove deploy and upgrade scripts

---
+++ scripts/main_deploy.ts
@@ -0,0 +1,48 @@
+import { expect } from "chai";
+import { ethers, upgrades, run } from "hardhat";
+import { getImplementationAddress } from "@openzeppelin/upgrades-core";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const initialOwner = process.env.SEPOLIA_INITIAL_OWNER;
+const uniswapRouter = process.env.SEPOLIA_UNISWAP_ROUTER;
+const liquidityReceiver = process.env.SEPOLIA_LIQUIDITY_RECEIVER;
+const treasuryReceiver = process.env.SEPOLIA_TREASURY_RECEIVER;
+const essrReceiver = process.env.SEPOLIA_ESSR_RECEIVER;
+
+async function main() {
+  const InstanceFactory = await ethers.getContractFactory("Eutopia");
+
+  const instance = await upgrades.deployProxy(InstanceFactory, [
+    initialOwner,
+    uniswapRouter,
+    liquidityReceiver,
+    treasuryReceiver,
+    essrReceiver
+  ]);
+  await instance.waitForDeployment();
+
+  const instanceAddress = await instance.getAddress();
+  console.log("Proxy deployed to " + instanceAddress);
+
+  const implementationAddress = await getImplementationAddress(ethers.provider, instanceAddress);
+  console.log("Implementation deployed to " + implementationAddress);
+
+  try {
+    await run("verify:verify", {
+      address: implementationAddress,
+      constructorArguments: [],
+    });
+    console.log("Implementation verified on Etherscan");
+  } catch (error) {
+    console.error("Error verifying :", error);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/main_upgrade.ts
@@ -0,0 +1,38 @@
+import { ethers, upgrades, run } from "hardhat";
+
+async function main() {
+    const proxyAddress = "0x9A2BAF3997A80ffCf303C8e5BB35f71c600fB362";
+
+    console.log("Upgrading...");
+
+    const InstanceFactory = await ethers.getContractFactory("Eutopia");
+    const upgraded = await upgrades.upgradeProxy(proxyAddress, InstanceFactory);
+
+    await upgraded.waitForDeployment();
+
+    await new Promise(resolve => setTimeout(resolve, 10000)); // //
+
+    const instanceAddress = await upgraded.getAddress();
+    const implementationAddress = await upgrades.erc1967.getImplementationAddress(instanceAddress);
+
+    console.log("Upgraded");
+    console.log("Instance address:", instanceAddress);
+    console.log("Implementation address:", implementationAddress);
+
+    try {
+        await run("verify:verify", {
+            address: implementationAddress,
+            constructorArguments: [],
+        });
+        console.log("Implementation verified on Etherscan");
+    } catch (error) {
+        console.error("Error verifying :", error);
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
